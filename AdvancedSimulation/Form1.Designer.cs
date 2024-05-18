2ac2597befd341b693cb2d3d5b498549fbcc0246
Adam <Adam@DESKTOP-BMVGUAK>
2024-05-18 22:34:21 +0200

    Dodaj pliki projektów.

---
+++ AdvancedSimulation/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace AdvancedSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.comboBoxColorScheme = new System.Windows.Forms.ComboBox();
+            this.trackBarTimerInterval = new System.Windows.Forms.TrackBar();
+            this.labelTimerInterval = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarTimerInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(499, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 35);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Uruchom symulację";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(499, 54);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 35);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Zakończ symulację";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(499, 95);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 35);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(607, 13);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(73, 13);
+            this.lblTimer.TabIndex = 4;
+            this.lblTimer.Text = "Czas trwania: ";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(499, 136);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 35);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Zapisz";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(499, 177);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 35);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Wczytaj";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxColorScheme
+            // 
+            this.comboBoxColorScheme.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxColorScheme.FormattingEnabled = true;
+            this.comboBoxColorScheme.Items.AddRange(new object[] {
+            "Standardowy",
+            "Pastelowy",
+            "Nocny"});
+            this.comboBoxColorScheme.Location = new System.Drawing.Point(593, 191);
+            this.comboBoxColorScheme.Name = "comboBoxColorScheme";
+            this.comboBoxColorScheme.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxColorScheme.TabIndex = 7;
+            this.comboBoxColorScheme.SelectedIndexChanged += new System.EventHandler(this.comboBoxColorScheme_SelectedIndexChanged);
+            // 
+            // trackBarTimerInterval
+            // 
+            this.trackBarTimerInterval.LargeChange = 50;
+            this.trackBarTimerInterval.Location = new System.Drawing.Point(590, 84);
+            this.trackBarTimerInterval.Maximum = 1000;
+            this.trackBarTimerInterval.Minimum = 10;
+            this.trackBarTimerInterval.Name = "trackBarTimerInterval";
+            this.trackBarTimerInterval.Size = new System.Drawing.Size(178, 45);
+            this.trackBarTimerInterval.SmallChange = 10;
+            this.trackBarTimerInterval.TabIndex = 8;
+            this.trackBarTimerInterval.TickFrequency = 50;
+            this.trackBarTimerInterval.Value = 10;
+            this.trackBarTimerInterval.ValueChanged += new System.EventHandler(this.trackBarTimerInterval_ValueChanged);
+            // 
+            // labelTimerInterval
+            // 
+            this.labelTimerInterval.AutoSize = true;
+            this.labelTimerInterval.Location = new System.Drawing.Point(607, 65);
+            this.labelTimerInterval.Name = "labelTimerInterval";
+            this.labelTimerInterval.Size = new System.Drawing.Size(83, 13);
+            this.labelTimerInterval.TabIndex = 9;
+            this.labelTimerInterval.Text = "Interwał timera: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AdvancedSimulation.Properties.Resources.istockphoto_1354565720_612x612;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTimerInterval);
+            this.Controls.Add(this.trackBarTimerInterval);
+            this.Controls.Add(this.comboBoxColorScheme);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Advanced Simulation - Conway";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarTimerInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ComboBox comboBoxColorScheme;
+        private System.Windows.Forms.TrackBar trackBarTimerInterval;
+        private System.Windows.Forms.Label labelTimerInterval;
+    }
+}
+
---
+++ AdvancedSimulation/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AdvancedSimulation
+{
+    public partial class Form1 : Form
+    {
+        private Cell[,] cells;  // Deklaracja tablicy komórek
+        private int rows = 80;
+        private int cols = 80;
+        private Timer timer;
+        private int cellSize = 10;
+        private Stopwatch stopwatch = new Stopwatch();  // Tylko jedna deklaracja
+
+        public Form1()
+        {
+            InitializeComponent(); // Inicjalizacja kontrolek formularza, w tym timer
+            trackBarTimerInterval.Minimum = 10;
+            trackBarTimerInterval.Maximum = 1000;
+            timer = new Timer();
+            int initialInterval = 100;
+            timer.Interval = initialInterval;  // Milisekundy
+            timer.Tick += Timer_Tick;
+
+            // Ustawienia dla TrackBar
+            trackBarTimerInterval.Value = initialInterval;
+            labelTimerInterval.Text = $"Interwał timera: {timer.Interval} ms";
+
+            // Przypisanie obsługi zdarzeń do przycisków
+            btnStart.Click += btnStart_Click;
+            btnStop.Click += btnStop_Click;
+            btnReset.Click += btnReset_Click;
+            btnSave.Click += btnSave_Click;
+            btnLoad.Click += btnLoad_Click;
+
+            cells = new Cell[rows, cols];  // Inicjalizacja tablicy komórek
+            pictureBox1.Paint += PictureBox1_Paint;
+            comboBoxColorScheme.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxColorScheme.DrawMode = DrawMode.OwnerDrawFixed;
+            comboBoxColorScheme.DrawItem += comboBoxColorScheme_DrawItem;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            InitializeGame();
+            timer.Start();
+            stopwatch.Restart();  // Rozpoczyna lub resetuje i rozpoczyna pomiar czasu
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            stopwatch.Stop();  // Zatrzymuje pomiar czasu
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop(); // Zatrzymuje timer symulacji
+            stopwatch.Reset(); // Resetuje i zatrzymuje Stopwatch
+            ClearCells(); // Czyści stan gry
+            pictureBox1.Invalidate(); // Odświeża PictureBox, aby pokazać czysty stan
+            UpdateTimerLabel(); // Resetuje tekst etykiety z czasem, jeśli istnieje
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            // Zapisuje '1' dla żywych komórek i '0' dla martwych
+                            sw.Write(cells[i, j].IsAlive ? '1' : '0');
+                        }
+                        sw.WriteLine(); // Nowa linia dla każdego wiersza
+                    }
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                {
+                    int y = 0;
+                    while (!sr.EndOfStream && y < rows)
+                    {
+                        string line = sr.ReadLine();
+                        for (int x = 0; x < Math.Min(line.Length, cols); x++)
+                        {
+                            cells[y, x] = new Cell(line[x] == '1');
+                        }
+                        y++;
+                    }
+                }
+                pictureBox1.Invalidate(); // Odświeża PictureBox po wczytaniu nowego stanu
+            }
+        }
+
+        private void InitializeGame()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    // Zmniejsz prawdopodobieństwo, że komórka będzie żywa
+                    cells[i, j] = new Cell(rand.Next(10) == 0); // 10% szansa, że komórka będzie żywa
+                }
+            }
+            pictureBox1.Invalidate();  // Wymuszenie odświeżenia na początku
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Cell[,] newCells = new Cell[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int aliveNeighbors = CountAliveNeighbors(i, j);
+                    bool isAlive = cells[i, j].IsAlive;
+
+                    // Klasyczne reguły gry w życie Conwaya
+                    bool survives = isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3);
+                    bool born = !isAlive && (aliveNeighbors == 3);
+
+                    newCells[i, j] = new Cell(survives || born);
+
+                    if (survives || born)
+                    {
+                        newCells[i, j].Age = cells[i, j].Age + 1;  // Zwiększ wiek, jeśli komórka przeżywa
+                    }
+                    else
+                    {
+                        newCells[i, j].Age = 0;  // Resetuj wiek
+                    }
+                }
+            }
+
+            cells = newCells;
+            pictureBox1.Invalidate();
+            UpdateTimerLabel();
+        }
+
+        private int CountAliveNeighbors(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    int nx = x + i;
+                    int ny = y + j;
+
+                    if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && cells[nx, ny].IsAlive)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Cell cell = cells[i, j];
+                    Color color = GetColorForCell(cell);
+                    Brush brush = new SolidBrush(color);
+
+                    g.FillRectangle(brush, j * cellSize, i * cellSize, cellSize, cellSize);
+                    g.DrawRectangle(Pens.Black, j * cellSize, i * cellSize, cellSize, cellSize);
+                }
+            }
+        }
+
+        private void ClearCells()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    cells[i, j] = new Cell(false, 0);  // Resetuje komórki
+                }
+            }
+        }
+
+        public class Cell
+        {
+            public bool IsAlive { get; set; }
+            public int Age { get; set; }
+
+            public Cell(bool isAlive, int age = 0)
+            {
+                IsAlive = isAlive;
+                Age = age;
+            }
+        }
+
+
+        private Color GetColorForCell(Cell cell)
+        {
+            if (cell == null || !cell.IsAlive)
+                return Color.White;
+
+            int ageFactor = Math.Min(255, cell.Age * 10);
+
+            string colorScheme = comboBoxColorScheme?.SelectedItem?.ToString() ?? "Standardowy"; // Ustawienie wartości domyślnej
+
+            switch (colorScheme)
+            {
+                case "Standardowy":
+                    return Color.FromArgb(255, 0, ageFactor, 0);
+                case "Pastelowy":
+                    return Color.FromArgb(255, Math.Min(255, ageFactor + 80), Math.Min(255, ageFactor + 160), 200);
+                case "Nocny":
+                    return Color.FromArgb(255, 0, 0, ageFactor);
+                default:
+                    return Color.FromArgb(255, 0, ageFactor, 0);
+            }
+        }
+        private void UpdateTimerLabel()
+        {
+            if (lblTimer != null)
+                lblTimer.Text = $"Czas trwania: {stopwatch.ElapsedMilliseconds} ms";
+        }
+        private void trackBarTimerInterval_ValueChanged(object sender, EventArgs e)
+        {
+            timer.Interval = trackBarTimerInterval.Value;
+            labelTimerInterval.Text = $"Interwał timera: {timer.Interval} ms";
+        }
+        private void comboBoxColorScheme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxColorScheme.SelectedItem.ToString())
+            {
+                case "Standardowy":
+                    // Ustaw standardowe kolory
+                    break;
+                case "Pastelowy":
+                    // Ustaw pastelowe kolory
+                    break;
+                case "Nocny":
+                    // Ustaw nocne kolory
+                    break;
+            }
+            pictureBox1.Invalidate(); // Przemaluj planszę z nowymi kolorami
+        }
+
+        private void comboBoxColorScheme_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+
+            // Pobierz element do narysowania
+            string text = comboBoxColorScheme.Items[e.Index].ToString();
+
+            // Rysowanie tła
+            e.DrawBackground();
+
+            // Ustawienie kolorów tekstu
+            Brush brush = ((e.State & DrawItemState.Selected) == DrawItemState.Selected) ?
+                          new SolidBrush(SystemColors.HighlightText) : new SolidBrush(e.ForeColor);
+
+            // Rysowanie tekstu
+            e.Graphics.DrawString(text, e.Font, brush, e.Bounds, StringFormat.GenericDefault);
+
+            // Rysowanie ramki dla aktywnego elementu
+            e.DrawFocusRectangle();
+        }
+
+        private void trackBarTimerInterval_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
